9f9828e500583dde2eeb5f6d8614fc0fb0f5a0c1
nicholasptran <nickptran@gmail.com>
2026-02-13 19:23:15 -0800

    added example and setup guide

---
+++ README.md
@@ -0,0 +1,21 @@
+# setup
+
+## dependencies
+
+[uv - python project manager](https://github.com/astral-sh/uv)
+
+## environment
+
+run this in your console:
+
+```powershell
+git clone https://github.com/nicholasptran/DocumentParser.git
+cd DocumentParser
+code .
+
+# this installs python if needed and installs dependencies into local environment
+uv sync
+
+# run this to see the console app
+uv run main.py
+```
--- parser2.py
+++ parser2.py
@@ -2,8 +2,19 @@ import pymupdf.layout
 import pymupdf4llm
 from pathlib import Path
 
-doc = pymupdf.open("documents/testing-cms1500.pdf")
-json = pymupdf4llm.to_json(doc, pages=[0])
-suffix = ".json"
+def parse(document: str):
+    doc = pymupdf.open(document)
 
-Path(doc.name).with_suffix(suffix).write_bytes(json.encode())
+    json = pymupdf4llm.to_json(doc)
+    md = pymupdf4llm.to_markdown(doc)
+    text = pymupdf4llm.to_text(doc)
+    Path(doc.name).with_suffix(".json").write_bytes(json.encode())
+    Path(doc.name).with_suffix(".md").write_bytes(md.encode())
+    Path(doc.name).with_suffix(".txt").write_bytes(text.encode())
+
+    doc.close()
+
+
+
+if __name__ == "__main__":
+    parse("documents/testing-cms1500.pdf")
